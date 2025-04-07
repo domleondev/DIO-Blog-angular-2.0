5fdb9effbccdd92e40a6a0a1a8db9fab2db55486
domleondev <contato.leon@live.com>
2025-04-07 06:16:11 -0300

    first commit

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Decola Tech 2025",
+    "description": "O programa contará com uma trilha educacional online focada em Spring Boot e Angular, onde as pessoas candidatas poderão aprender desde o básico até criarem e publicarem suas próprias APIs, se tornando full stacks e desenvolvendo projetos práticos junto com experts. A trilha completa terá 67 horas de duração com certificação para os concluintes.",
+    "photoCover":"https://hermes.digitalinnovation.one/tracks/94bed9ab-616c-4982-8a98-a6150ece419c.png"
+  },
+  {
+    "id":"2",
+    "title": "Solo Leveling Ultrapassa One Piece e Kimetsu no Yaiba",
+    "description": "O anime Solo Leveling alcançou um marco significativo ao se tornar o mais avaliado na plataforma de streaming Crunchyroll, superando títulos consagrados como One Piece e Kimetsu no Yaiba.",
+    "photoCover":"https://imageio.forbes.com/specials-images/imageserve/67bc8ceddfce63fdaf8b75f8/solo-leveling/960x0.jpg?format=jpg&width=960"
+  },
+  {
+    "id":"3",
+    "title": "Microsoft e Avanade focam na transformação com IA Generativa e Copilot",
+    "description": "As empresas investem no lançamento da Prática de Transformação de Negócios Copilot, em novos recursos e treinamento de clientes para acelerar e dimensionar a adoção da IA.",
+    "photoCover":"https://midias.inforchannel.com.br/wp-content/uploads/2024/04/Robos-Computador-Foto-Freepik-1024x574.jpg.webp"
+  },
+  {
+    "id":"4",
+    "title": "DIO oferece 75 mil bolsas gratuitas para aprender Inteligência Artificial com a Microsoft",
+    "description": "A Microsoft e a DIO lançam o programa Microsoft AI for Tech, uma iniciativa que é parte do programa Microsoft ConectAI que tem o objetivo de capacitar 5 milhões de pessoas em habilidades de inteligência artificial (IA) até 2028, preparando-as para o futuro do trabalho.",
+    "photoCover":"https://assets.dio.me/wcg9bgTD7vtqBcAXRfsTuD2TYdauPjlN3y6MOOQGaPw/f:webp/q:80/w:516/L3RyYWNrcy9jb3Zlci83OTFkNWVmMi05NzhmLTRhYmMtOTMwZC1mY2IyYzhjMTNjYjUucG5n"
+  }
+]
